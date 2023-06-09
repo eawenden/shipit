8d28941639e97bfb397b07c5f1b31e84d6d82659
Erik van der Wenden <eawenden@users.noreply.github.com>
2023-06-09 15:31:54 +0200

    Update RetrieveQuestion.kt

--- src/main/kotlin/net/dpgmedia/services/RetrieveQuestion.kt
+++ src/main/kotlin/net/dpgmedia/services/RetrieveQuestion.kt
@@ -48,7 +48,7 @@ object RetrieveQuestion {
             Do it in Dutch.
             Do not include any explanations, only provide a  RFC8259 compliant JSON response  following this format without deviation.
             {
-              "question": "language of original phrase",
+              "question": "the question text",
               "options": [{
                 "id": "the letter associated with the answer",
                 "label": "the answer"
